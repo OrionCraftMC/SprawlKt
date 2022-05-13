4db241b5517c6533fccfa76e9fa1ca2de7da887f
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-05-13 13:13:15 +0100

    Clean-up

--- src/main/kotlin/io/github/orioncraftmc/stretchkt/forest/algo/ForestExtensions.kt
+++ src/main/kotlin/io/github/orioncraftmc/stretchkt/forest/algo/ForestExtensions.kt
@@ -6,26 +6,26 @@ import io.github.orioncraftmc.stretchkt.result.Cache
 import kotlin.math.round
 
 internal fun Forest.roundLayout(
-root: NodeData,
-absX: Float,
-absY: Float,
+    root: NodeData,
+    absX: Float,
+    absY: Float,
 ) {
-    val layout = root.layout;
+    val layout = root.layout
     val finalAbsX = absX + layout.location.x
     val finalAbsY = absY + layout.location.y
 
-    layout.location.x = round(layout.location.x);
-    layout.location.y = round(layout.location.y);
+    layout.location.x = round(layout.location.x)
+    layout.location.y = round(layout.location.y)
 
-    layout.size.width = round(finalAbsX + layout.size.width) - round(finalAbsX);
-    layout.size.height = round(finalAbsY + layout.size.height) - round(finalAbsY);
+    layout.size.width = round(finalAbsX + layout.size.width) - round(finalAbsX)
+    layout.size.height = round(finalAbsY + layout.size.height) - round(finalAbsY)
 
     for (child in root.children) {
-        roundLayout(child, finalAbsX, finalAbsY);
+        roundLayout(child, finalAbsX, finalAbsY)
     }
 }
 
-internal fun Forest.cache(node: NodeData, mainSize: Boolean): Cache? {
+internal fun cache(node: NodeData, mainSize: Boolean): Cache? {
     return if (mainSize) {
         node.mainSizeLayoutCache
     } else {
@@ -33,7 +33,7 @@ internal fun Forest.cache(node: NodeData, mainSize: Boolean): Cache? {
     }
 }
 
-internal fun Forest.setCache(node: NodeData, mainSize: Boolean, cache: Cache?) {
+internal fun setCache(node: NodeData, mainSize: Boolean, cache: Cache?) {
     if (mainSize) {
         node.mainSizeLayoutCache = cache
     } else {
